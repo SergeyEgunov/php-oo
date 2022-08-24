7bda1d1bdc84ed148dde34c1c39fb5a779c72735
SergeyEgunov <109955909+SergeyEgunov@users.noreply.github.com>
2022-08-24 15:07:28 +0300

    Update ops.php

--- ops.php
+++ ops.php
@@ -38,17 +38,23 @@ class Person{
   }
   function getInfo(){
     return "
-    <h3>A few words about myself</h3><br>"."My name is ".$this->getName()."<br>"."my mother is ".$this->getMother()->name."<br>"."my father is ".$this->getFather()->name
+    <h3>A few words about myself</h3><br>"."My name is ".$this->getName()."<br>"."my mother is ".$this->getMother()->name."<br>"."my father is ".$this->getFather()->name."<br>"."My grandmothers is ".$this->getMother()->getMother()->name." and ".$this->getFather()->getMother()->name."<br>".
+    "My grandfathers is ".$this->getMother()->getFather()->name." and ".$this->getFather()->getFather()->name
     ;
   }
 }
 // $medKit = 50;
-$alex = new Person("Alex", "Ivanov", 42);
-$olga = new Person("Olga", "Ivanova", 42, null, $igor);
-$valera = new Person("Valera", "Ivanov", 15, $olga, $alex);
-
 $igor = new Person("Igor", "Petrov", 68);
+$nata = new Person("Nata", "Petrova", 68);
+$petr = new Person("Petr", "Ivanov", 68);
+$alisa = new Person("Alisa", "Ivanova", 68);
+$alex = new Person("Alex", "Ivanov", 42, $alisa, $petr);
+$olga = new Person("Olga", "Ivanova", 42, $nata, $igor);
+$valera = new Person("Valera", "Ivanov", 15, $olga, $alex, );
+
+
+
+
+
 
 echo $valera->getInfo();
-
-//<h3>A few words about myself</h3><br>"."My name is ".$this->getName()."<br>"."my mother is".$this->getMother()."<br>"."my father is".$this->getFather()
